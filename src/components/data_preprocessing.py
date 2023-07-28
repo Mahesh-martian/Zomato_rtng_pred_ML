557619eb3f0960276f495314d56ae68b461c7eb6
Mahesh-martian <maheshkumar201095@gmail.com>
2023-07-28 10:18:07 +0530

    First commit

---
+++ src/components/data_preprocessing.py
@@ -0,0 +1,7 @@
+# To activate this environment, use
+#
+#     $ conda activate "D:\F drive\learning\Besant_tech\Data_science_batch\6.EDA and Features Engineering\Demo_project_pipeline\Zomato_rating_prediction\Zomato_rtng_pred_env"
+#
+# To deactivate an active environment, use
+#
+#     $ conda deactivate
